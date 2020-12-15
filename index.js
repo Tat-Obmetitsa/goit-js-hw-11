4e429463cdb6ca8bc429158e7e3370c5ef7eab48
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2020-12-15 22:44:00 +0200

    adds promisification tasks

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import './color-switch.js';
+import './js/color-switch.js';
+import './js/timer.js';
+import './js/promisification/task-1.js';
+import './js/promisification/task-2.js';
+import './js/promisification/task-3.js';
