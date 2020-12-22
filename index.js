8f0417f8c0d0e178f0a823e90ae948c73e9103d5
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2020-12-22 20:03:07 +0200

    bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import './js/color-switch.js';
-import './js/timer.js';
-import './js/promisification/task-1.js';
-import './js/promisification/task-2.js';
+// import './js/color-switch.js';
+// import './js/timer.js';
+// import './js/promisification/task-1.js';
+// import './js/promisification/task-2.js';
 import './js/promisification/task-3.js';
--- js/promisification/task-3.js
+++ js/promisification/task-3.js
@@ -1,4 +1,3 @@
-// task-3
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -33,4 +32,3 @@ makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
 makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
-
