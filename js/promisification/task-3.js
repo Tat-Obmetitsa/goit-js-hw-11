0aad856f0792d3dc39e103e758883972d88dcd81
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2020-12-22 19:58:11 +0200

    adds fixes

--- js/promisification/task-3.js
+++ js/promisification/task-3.js
@@ -1,8 +1,7 @@
-// task 3
+// task-3
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
 
 const makeTransaction = transaction => {
   return new Promise((resolve, reject) => {
@@ -34,3 +33,4 @@ makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
 makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+
